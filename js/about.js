473d91775cd9359c0dae90383c8070e36b96c3c8
SinghSujitkumar <2017.sujitkumar.singh@ves.ac.in>
2020-05-10 15:14:35 +0530

    included svg removed fa

--- js/about.js
+++ js/about.js
@@ -5,7 +5,7 @@ function renderExperience(doc){
     let comp = document.createElement('h1');
     let role = document.createElement('h2');
     let certi = document.createElement('a');
-    certi.innerHTML = "<i class='fas fa-2x fa-file-pdf'></i>";
+    certi.innerHTML = ' <svg style = "width:10%;height:2%;" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M13 21l2-1l2 1v-7h-4m4-5V7l-2 1l-2-1v2l-2 1l2 1v2l2-1l2 1v-2l2-1m1-7H4a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h7v-2H4V5h16v10h-1v2h1a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2m-9 5H5V6h6m-2 5H5V9h4m2 5H5v-2h6z" fill="#ffffff"/></svg>';
     certi.setAttribute('target','_blank');
     certi.setAttribute('href',doc.data().certi);
     let duration = document.createElement('h4')
